65bb78ea9036fd9ad71cb1f995faac640929db4f
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-01-28 01:02:48 +0500

    updated

--- src/Common/Ourfligths.jsx
+++ src/Common/Ourfligths.jsx
@@ -15,8 +15,8 @@ const Ourfligths = () => {
   };
 
   return (
-    <div className='h-screen w-full   xl:py-6 xl:px-13 lg:px-14  lg:py-6 md:py-5  py-2 flex items-end justify-end fixed z-999'>
-      <div className='lg:min-h-[30vh]  min-h-[60vh] md:min-h-[20vh] flex items-end justify-end  w-full  relative '>
+    <div className='h-screen w-full   xl:py-6  xl:px-13 lg:px-14  lg:py-6 md:py-5   py-2 flex items-end justify-end fixed z-999'>
+      <div className='lg:min-h-[30vh] lg:py-4  min-h-[60vh] md:min-h-[20vh] flex items-end justify-end  w-full  relative '>
   
   {/* Our fight button */}
 
@@ -31,7 +31,7 @@ const Ourfligths = () => {
     text-white
     transition-all duration-300 ease-out
     active:scale-[0.97]'>
-      <div className='cursor-pointer   flex items-center  w-full xl:p-1  xl:px-4 md:px-5 md:py-2 px-4 py-1 lg:px-6 rounded-full  justify-center bg-white lg:to-blue-300  text-black'>
+      <div className='cursor-pointer   flex items-center  w-full xl:p-1  xl:px-4 md:px-5 md:py-2 px-4 py-1 lg:px-6 rounded-full  justify-center bg-white   text-black'>
       <h2 className='Gt2 xl:text-[1.1vw] lg:text-[1.7vw] md:text-2xl text-[3vw] mt-1 tracking-tighter '>Book the flight</h2>
       </div>
  
