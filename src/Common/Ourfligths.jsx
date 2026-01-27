37df0b0b77743468494cc78948be0180a8e7a56c
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-01-28 00:20:21 +0500

    ypdated

--- src/Common/Ourfligths.jsx
+++ src/Common/Ourfligths.jsx
@@ -15,13 +15,13 @@ const Ourfligths = () => {
   };
 
   return (
-    <div className='h-screen w-full   lg:py-6 lg:px-13 flex items-end justify-end fixed z-999'>
-      <div className='lg:min-h-[30vh] min-h-[60vh]  flex items-end justify-end  w-full  relative '>
+    <div className='h-screen w-full   xl:py-6 xl:px-13 lg:px-14  lg:py-6 md:py-5  py-2 flex items-end justify-end fixed z-999'>
+      <div className='lg:min-h-[30vh]  min-h-[60vh] md:min-h-[20vh] flex items-end justify-end  w-full  relative '>
   
   {/* Our fight button */}
 
   <div className='h-full  w-full    flex items-end justify-center  ' >
-    <button onClick={buttonHandler} className='p-1 px-2
+    <button onClick={buttonHandler} className='xl:p-2 xl:py-2 xl:px-3 lg:p-2 lg:px-4 p-1 py-2  px-2 
     flex items-center justify-between gap-1
     rounded-full
     backdrop-blur-md
@@ -31,12 +31,12 @@ const Ourfligths = () => {
     text-white
     transition-all duration-300 ease-out
     active:scale-[0.97]'>
-      <div className='cursor-pointer  flex items-center  w-full p-1 px-4 rounded-full  justify-center bg-white text-black'>
-      <h2 className='Gt2 lg:text-[1.1vw]  mt-1 tracking-tighter '>Book the flight</h2>
+      <div className='cursor-pointer   flex items-center  w-full xl:p-1  xl:px-4 md:px-5 md:py-2 px-4 py-1 lg:px-6 rounded-full  justify-center bg-white lg:to-blue-300  text-black'>
+      <h2 className='Gt2 xl:text-[1.1vw] lg:text-[1.7vw] md:text-2xl text-[3vw] mt-1 tracking-tighter '>Book the flight</h2>
       </div>
  
        <div className='p-2 rounded-full  bg-white text-black'>
-       <IoIosSend className='cursor-pointer text-1xl' />
+       <IoIosSend className='cursor-pointer xl:text-[1.4vw] md:text-3xl lg:text-2xl' />
        </div>
     </button>
   </div>
@@ -46,17 +46,17 @@ const Ourfligths = () => {
 
  {
   Openpopup && (
-    <div className='h-full w-full opacity-100 lg:rounded-3xl flex items-center justify-start overflow-hidden lg:flex-row flex-col absolute top-0 left-0 lg:bg-white bg-white'>
+    <div className='h-full w-full lg:py-0 py-3 opacity-100 lg:rounded-3xl flex items-center justify-start overflow-hidden lg:flex-row flex-col absolute top-0 left-0 lg:bg-white bg-white'>
    
         <div onClick={Close}  className='py-3 px-3 w-fit  absolute top-0 right-0'>
         <IoIosClose className='cursor-pointer text-4xl' />
 
         </div>
     <div className='h-full flex items-center justify-center px-5  '>
-      <h2 className='Gt3 text-3xl'>Contact</h2>
+      <h2 className='Gt3 text-3xl md:text-5xl  xl:text-4xl'>Contact</h2>
     </div>
     <div className=' h-full w-full   px-3'>
-        <form action="" className='h-full flex items-center justify-start lg:flex-row flex-col lg:gap-12 gap-5 w-full '> 
+        <form action="" className='h-full flex items-center justify-start lg:flex-row flex-col md:flex-row lg:gap-12 gap-5 w-full '> 
         <TextField
          id="filled-multiline-flexible"
          label="Name"
@@ -103,7 +103,7 @@ const Ourfligths = () => {
        
        />
  <div className='p-2 rounded-full  bg-black text-white'>
-       <IoIosSend className='text-2xl' />
+       <IoIosSend className='text-2xl md:text-3xl xl:text-2xl ' />
        </div>
         </form>
     </div>
